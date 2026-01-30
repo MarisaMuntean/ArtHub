add0a82b72eb66b72ad1988d8facde4ab999da6b
MarisaMuntean <munteanmarisa@gmail.com>
2026-01-30 13:14:20 +0200

    Delete src/main/resources/data.sql

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (username, password, role) 
-VALUES ('editor', '$2a$10$2uzVbPYB8LK.Icro7ICuGuIRNy8W1NUmJ1k2YIkGLBwR52lHrYPGe', 'EDITOR');
-
-INSERT INTO users (username, password, role) 
-VALUES ('user', '$2a$10$2uzVbPYB8LK.Icro7ICuGuIRNy8W1NUmJ1k2YIkGLBwR52lHrYPGe', 'USER');
